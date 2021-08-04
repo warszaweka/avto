b2c0829ebf5022f3e38bd7107e33ecea87d1a9ca
Kyrylo Androsiuk <warszaweka@tuta.io>
2021-08-04 19:19:53 +0300

    Fixed pickle problem

--- main.py
+++ main.py
@@ -11,31 +11,37 @@ if database_url.startswith("postgres://"):
     database_url = database_url.replace(
         "postgres://", "postgresql+psycopg2://", 1
     )
-engine = create_engine(database_url, future=True)
-
-bot = Bot(os.getenv("API_TOKEN"))
-
+api_token = os.getenv("API_TOKEN")
 webhook_token = os.getenv("WEBHOOK_TOKEN")
-
 redis_url = os.getenv("REDIS_URL")
 
+
+def get_bot():
+    if not hasattr(get_bot, "bot"):
+        get_bot.bot = Bot(api_token)
+    return get_bot.bot
+
+
 flask = Flask(__name__)
-print("MAIN")  # debug
 
 
 @flask.route(f"/{webhook_token}", methods=["POST"])
 def flask_handler():
-    print("FLASK")  # debug
-    update = Update.de_json(request.get_json(), bot)
+    update = Update.de_json(request.get_json(), get_bot())
     Queue("default", connection=from_url(redis_url)).enqueue(
         redis_handler, update
     )
 
 
 def redis_handler(update: Update):
-    print("REDIS")  # debug
+    def get_engine():
+        if not hasattr(redis_handler, "engine"):
+            redis_handler.engine = create_engine(database_url, future=True)
+        return redis_handler.engine
+
     if hasattr(update, "message") and hasattr(update.message, "text"):
         reply_text = update.message.text
-        bot.send_message(update.message.chat.id, f"TEST: {reply_text}")
-    with engine.connect() as connection:
+        get_bot().send_message(update.message.chat.id, f"TEST: {reply_text}")
+
+    with get_engine.connect() as connection:
         connection.execute(text("select 'worker test'")).all()
