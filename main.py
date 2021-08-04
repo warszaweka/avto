51519d8b40af7457516bc02e2c4142f46a27589b
Kyrylo Androsiuk <warszaweka@tuta.io>
2021-08-04 17:49:46 +0300

    Telegram test

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
 from redis import from_url
 from rq import Queue
 from sqlalchemy import create_engine, text
+from telegram import Bot, Update
 
 database_url = os.getenv("DATABASE_URL")
 if database_url.startswith("postgres://"):
@@ -11,6 +12,8 @@ if database_url.startswith("postgres://"):
         "postgres://", "postgresql+psycopg2://", 1
     )
 engine = create_engine(database_url, future=True)
+
+bot = Bot(os.getenv("API_TOKEN"))
 
 webhook_token = os.getenv("WEBHOOK_TOKEN")
 
@@ -21,10 +24,15 @@ flask = Flask(__name__)
 
 @flask.route(f"/{webhook_token}")
 def flask_handler():
-    Queue('default', connection=from_url(redis_url)).enqueue(redis_handler)
-    return "<p>main test</p>"
+    update = Update.de_json(request.get_json(), bot)
+    Queue("default", connection=from_url(redis_url)).enqueue(
+        redis_handler, update
+    )
 
 
-def redis_handler():
+def redis_handler(update: Update):
+    if hasattr(update, "message") and hasattr(update.message, "text"):
+        reply_text = update.message.text
+        bot.send_message(update.message.chat.id, f"TEST: {reply_text}")
     with engine.connect() as connection:
         connection.execute(text("select 'worker test'")).all()
