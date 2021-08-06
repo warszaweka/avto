364bdf4b59f8f86b01ab178151567fbb2f2c17bb
Kyrylo Androsiuk <warszaweka@tuta.io>
2021-08-06 16:06:36 +0300

    models column type fix

--- models.py
+++ models.py
@@ -8,6 +8,6 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "state"
 
-    id = Column(type=Integer, primary_key=True)
-    state_id = Column(type=String(64), nullable=False)
-    state_args = Column(type=JSON)
+    id = Column(Integer, primary_key=True)
+    state_id = Column(String(64), nullable=False)
+    state_args = Column(JSON)
