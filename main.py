da03ee99c05397e9d74f4fcc274ec649f56a935b
Kyrylo Androsiuk <warszaweka@tuta.io>
2021-08-08 13:45:25 +0300

    reply bot

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ from re import sub
 
 from flask import Flask, request
 from gevent import spawn
-from requests import Response, request as requests_request
+from requests import Response
+from requests import request as requests_request
 from sqlalchemy.future import Engine, create_engine
+from sqlalchemy.orm import Session
 from sqlalchemy.schema import MetaData
 from ujson import dumps, loads
 
-from models import DeclarativeBase
+from models import DeclarativeBase, User
 
 engine = create_engine(
     sub(r"^[^:]*", "postgresql+psycopg2", getenv("DB_URL"), 1)
@@ -33,29 +35,23 @@ flask: Flask = Flask(__name__)
 @flask.route(f"/{getenv('WH_TOKEN')}", methods=["POST"])
 def flask_handler() -> tuple:
     spawn(tg_handler, request.data)
+
     return ("", 204)
 
 
 def tg_handler(data: bytes) -> None:
     data: dict = loads(data)
-    print(dumps(data, indent=2))
-    # if update.callback_query is not None:
-        # callback = True
-        # user_id = update.callback_query.from_user.id
-    # elif update.message is not None:
-        # callback = False
-        # user_id = update.message.from_user.id
-    # else:
-        # return
-    # with Session(engine) as session:
-        # user = session.get(User, user_id)
-        # if user is None:
-            # user_exists = False
-        # else:
-            # user_exists = True
-            # state_id = user.state_id
-            # state_args = user.state_args
-    # admin = user_id in admins
+
+    if "message" in data:
+        message = data["message"]
+        if "text" in message:
+            tg_request(
+                "sendMessage",
+                {
+                    "chat_id": message["chat"]["id"],
+                    "text": message["text"],
+                },
+            )
 
 
 def tg_request(method: str, data: dict) -> dict:
@@ -65,5 +61,7 @@ def tg_request(method: str, data: dict) -> dict:
         url=f"https://api.telegram.org/bot{tg_token}/{method}",
         data=dumps(data),
     )
+
     result.raise_for_status()
+
     return loads(result.content)
